0473914010633a9ad3607c4bdbf37f3cf5466e8e
AvanDriel <avandriel1998@gmail.com>
2019-09-09 17:21:20 +0200

    Added basic drag drop functionality with the inventory

--- Assets/Scripts/Automove.cs
+++ Assets/Scripts/Automove.cs
@@ -9,8 +9,20 @@ public class Automove : MonoBehaviour
     [Range(-5, 5)]
     public float Speed_Y;
 
+    public Inventory inventory;
+
     void Update()
     {
         this.transform.Translate(new Vector2(Speed_X, Speed_Y) * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        IInventoryItem item = collision.collider.GetComponent<IInventoryItem>();
+
+        if(item != null)
+        {
+            inventory.AddItem(item);
+        }
     }
 }
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Inventory Inventory;
+
+    private void Start()
+    {
+        Inventory.ItemAdded += Inventory_ItemAdded;
+        Inventory.ItemRemoved += Inventory_ItemRemoved;
+    }
+
+    public Transform inventoryPanel;
+
+    private void Inventory_ItemAdded(object sender, InventoryEventArgs e)
+    {
+        foreach (Transform Slot in inventoryPanel)
+        {
+            Image image = Slot.GetChild(0).GetComponent<Image>();
+            ItemDragHandler itemDragHandler = Slot.GetChild(0).GetComponent<ItemDragHandler>();
+
+            if(!image.enabled)
+            {
+                image.enabled = true;
+                image.sprite = e.Item.Image;
+                itemDragHandler.Item = e.Item;
+
+                break;
+            }
+        }
+    }
+
+    private void Inventory_ItemRemoved(object sender, InventoryEventArgs e)
+    {
+        foreach (Transform Slot in inventoryPanel)
+        {
+            Image image = Slot.GetChild(0).GetComponent<Image>();
+            ItemDragHandler itemDragHandler = Slot.GetChild(0).GetComponent<ItemDragHandler>();
+
+            if (itemDragHandler.Item.Equals(e.Item))
+            {
+                image.enabled = false;
+                image.sprite = null;
+                itemDragHandler.Item = null;
+
+                break;
+            }
+        }
+    }
+}
