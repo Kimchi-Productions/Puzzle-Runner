ad1b7f2b22511d68286c377ef3d189e9d99bb4f9
Maher <mahermx1@outlook.com>
2019-09-09 19:10:01 +0200

    still in progress

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenuUI;
+
+    public void Pause () {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume () {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+}
---
+++ Assets/Scripts/msdsjd.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class msdsjd : MonoBehaviour
+{
+    public bool paused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pause()
+    {
+        paused = true;
+
+        if (Time.timeScale == 0)
+        {
+            paused = false;
+        }
+
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
