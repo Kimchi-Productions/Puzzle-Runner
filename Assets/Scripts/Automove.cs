d44642d877d3bdfebd06e7565adb93d1de05b4f2
AvanDriel <avandriel1998@gmail.com>
2019-09-09 10:22:04 +0200

    Init

---
+++ Assets/Scripts/Automove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Automove : MonoBehaviour
+{
+    [Range(-5, 5)]
+    public float Speed_X;
+    [Range(-5, 5)]
+    public float Speed_Y;
+
+    void Update()
+    {
+        this.transform.Translate(new Vector2(Speed_X, Speed_Y) * Time.deltaTime);
+    }
+}
