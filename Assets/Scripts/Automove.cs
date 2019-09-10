852170a4e67b5dd8387db16eab271e5388264b0c
Binh An Nguyen <labinhan@hotmail.com>
2019-09-10 13:28:00 +0200

    changed values of player
    changed values of player and changed collision

--- Assets/Scripts/Automove.cs
+++ Assets/Scripts/Automove.cs
@@ -9,6 +9,22 @@ public class Automove : MonoBehaviour
     [Range(-5, 5)]
     public float Speed_Y;
 
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Destroy(this.gameObject);
+        Destroy(other.gameObject);
+        Debug.Log("Destroyed due to collision");
+    }
+
+
+    void OnBecameInvisible()
+    {
+        Destroy(this.gameObject);
+        Debug.Log("Destroyed due to despawning");
+    }
+
     void Update()
     {
         this.transform.Translate(new Vector2(Speed_X, Speed_Y) * Time.deltaTime);
