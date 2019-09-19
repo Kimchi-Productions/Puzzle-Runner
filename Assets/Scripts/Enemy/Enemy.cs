335d1255a97f1c262d8829f7e11ea01a78b65f47
Lennart <lennartjvw@icloud.com>
2019-09-19 10:51:01 +0200

    Added a shield and enemy + shoot script
    Shield is droppable from the inventory.
    Enemy should be placed somewhere in a level.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject projectile;
+    public float timer;
+    public int timeBetweenShooting = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeBetweenShooting)
+        {
+            Instantiate(projectile, this.transform.position, Quaternion.identity);
+            timer = 0;
+        }
+
+    }
+}
