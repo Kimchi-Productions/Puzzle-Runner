f7cc7a4fbbc5a93a3183022a706c737fb0b44fed
Lennart <lennartjvw@icloud.com>
2019-09-19 12:08:52 +0200

    Added a bomb item
    Also addeed a door that can be destroyed by the bomb item

---
+++ Assets/Scripts/Items/Bomb.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour, IInventoryItem
+{
+    public string _Name;
+    public string Name
+    {
+        get
+        {
+            return _Name;
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+
+    public void OnPickUp()
+    {
+
+        if (gameObject.tag == "CanPickUp")
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public float timer;
+    public int timeToDetonate = 2;
+    public bool detonated = false;
+    public GameObject door;
+
+    public void OnDrop()
+    {
+        gameObject.tag = "CantPickUp";
+        gameObject.SetActive(true);
+
+        Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pz.z = 0;
+        gameObject.transform.position = pz;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > timeToDetonate && detonated == false)
+        {
+            gameObject.transform.GetComponent<CircleCollider2D>().radius = 1.5f;
+            detonated = true;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == door)
+        {
+            Destroy(door);
+            Destroy(gameObject);
+        }
+    }
+
+}
