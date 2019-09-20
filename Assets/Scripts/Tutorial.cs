e2de6360ef92ba6ce7261aea1dac90be87afccaf
AvanDriel <avandriel1998@gmail.com>
2019-09-20 13:34:15 +0200

    Added tutorial

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    private bool hasStarted = false;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            Time.timeScale = 0f;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            Time.timeScale = 1f;
+        }
+    }
+}
