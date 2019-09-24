a68eb6ccae327591d9aae3392efee090dc389498
Shahraiz <sharaiz123@gmail.com>
2019-09-24 13:56:47 +0200

    item sounds

--- Assets/Scripts/Items/GravitySwitch.cs
+++ Assets/Scripts/Items/GravitySwitch.cs
@@ -62,6 +62,7 @@ public class GravitySwitch : MonoBehaviour, IInventoryItem
         if (collision.gameObject.tag == "Player" && gameObject.tag == "CantPickUp")
         {
             GameObject Player = GameObject.FindGameObjectWithTag("Player");
+            gameObject.GetComponent<AudioSource>().Play();
             Player.GetComponent<Rigidbody2D>().gravityScale = gravityAfterCollision;
             Player.transform.Rotate(900, 0, 0);
         }
