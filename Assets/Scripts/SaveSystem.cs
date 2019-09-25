9f372a389a9dddd8385936ae8961a762f09b0384
Lennart <lennartjvw@icloud.com>
2019-09-25 17:46:43 +0200

    Added save location for android

--- Assets/Scripts/SaveSystem.cs
+++ Assets/Scripts/SaveSystem.cs
@@ -7,17 +7,19 @@ using System.IO;
 public class SaveSystem
 {
     public static void SaveStar()
-    {
+    { 
         BinaryFormatter formatter = new BinaryFormatter();
-        FileStream stream = new FileStream("savedStars.dat", FileMode.Create);
+        FileStream stream = new FileStream(Application.persistentDataPath + "/savedStars.dat", FileMode.Create);
         formatter.Serialize(stream, StarManagement.starList);
         stream.Close();
+
+
     }
 
     public static void LoadStar()
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        FileStream file = File.Open("savedStars.dat", FileMode.Open);
+        FileStream file = File.Open(Application.persistentDataPath + "/savedStars.dat", FileMode.Open);
         StarManagement.starList = (List<Star>)formatter.Deserialize(file);
         file.Close();
     }
