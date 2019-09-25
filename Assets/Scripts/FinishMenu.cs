f10e64cefed775a3cc8729b0bf7bffc53d83d063
Binh Nguyen <labinhan@hotmail.com>
2019-09-25 10:45:19 +0200

    Revert "Feature/star system"

--- Assets/Scripts/FinishMenu.cs
+++ Assets/Scripts/FinishMenu.cs
@@ -1,38 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class FinishMenu : MonoBehaviour
 {
     public GameObject finishui;
-    public int getActiveLevel;
-    public int addedStarts;
-
     public void start(){
         Time.timeScale = 1f;
         finishui.SetActive(false);
-
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(ItemStar.starPickedUp == false)
-        {
-            getActiveLevel = SceneManager.GetActiveScene().buildIndex -2;
-            addedStarts = 2;
-            StarManagement.addStar(getActiveLevel, addedStarts);
-            Time.timeScale = 0f;
-            finishui.SetActive(true);
-        }
-        else
-        {
-            getActiveLevel = SceneManager.GetActiveScene().buildIndex -2;
-            addedStarts = 3;
-            StarManagement.addStar(getActiveLevel, addedStarts);
-            Time.timeScale = 0f;
-            finishui.SetActive(true);
-        }
-        
+        Time.timeScale = 0f;
+        finishui.SetActive(true);
     }
 }
