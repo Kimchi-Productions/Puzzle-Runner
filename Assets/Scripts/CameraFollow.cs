ea53de933203c1b6dcaad28c3184c1180a1c997b
AvanDriel <avandriel1998@gmail.com>
2019-09-25 12:27:25 +0200

    Update CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,22 +6,14 @@ public class CameraFollow : MonoBehaviour
 {
 
     public GameObject targetObject;
-    private float distanceToTarget;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        distanceToTarget = transform.position.x - targetObject.transform.position.x;
-    }
-
-    // Update is called once per frame
+ 
     void Update()
     {
         float targetObjectX = targetObject.transform.position.x;
         float targetObjectY = targetObject.transform.position.y;
         Vector3 newCameraPosition = transform.position;
-        newCameraPosition.x = targetObjectX + distanceToTarget - 1;
-        newCameraPosition.y = targetObjectY + distanceToTarget - 3.5f;
+        newCameraPosition.x = targetObjectX + 4;
+        newCameraPosition.y = targetObjectY;
         transform.position = newCameraPosition;
 
     }
