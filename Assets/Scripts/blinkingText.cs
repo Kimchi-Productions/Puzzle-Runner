1ef76a06fa0b0aba368d2728b7de7731c901ca35
Lennart <lennartjvw@icloud.com>
2019-10-07 15:52:49 +0200

    Added tutorial and star sprites

---
+++ Assets/Scripts/blinkingText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blinkingText : MonoBehaviour
+{
+
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = text.GetComponent<Text>();
+        StartCoroutine(BlinkText());
+    }
+
+    public IEnumerator BlinkText()
+    {
+        while (true)
+        {
+            text.text = "";
+            yield return new WaitForSeconds(.5f);
+            text.text = "TUTORIAL";
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+}
