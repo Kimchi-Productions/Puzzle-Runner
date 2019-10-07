20319e5350f8ff6c0b54eb052fe8424737083d9a
Alwin van Driel <avandriel1998@gmail.com>
2019-10-07 16:55:41 +0200

    Not able to pause bug fix

---
+++ Assets/Scripts/MenuOpened.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOpened : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.activeSelf == true)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
